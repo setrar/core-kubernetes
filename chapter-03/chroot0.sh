fff7711016adfad680d4ce1b315eaa7d12fa3ec9
setrar <setrar@valiha.com>
2022-06-26 10:52:57 -0400

    Update chroot0.sh

--- chapter-03/chroot0.sh
+++ chapter-03/chroot0.sh
@@ -1,21 +1,24 @@
 mkdir -p /home/namespace/box
 
-mkdir /home/namespace/box/bin
-mkdir /home/namespace/box/lib
-mkdir /home/namespace/box/proc
 
+# Populate Binaries
+mkdir /home/namespace/box/bin
 cp -v /usr/bin/kill /home/namespace/box/bin
 cp -v /usr/bin/ps /home/namespace/box/bin
 cp -v /bin/bash /home/namespace/box/bin
 cp -v /bin/ls /home/namespace/box/bin
 
+# Populate Libraries
+mkdir /home/namespace/box/lib
 cp -r /lib/ /home/namespace/box
 
-mount -t proc proc /home/namespace/box/proc 
-
-# uncomment to add data volume
+# Uncomment to populate data volume
 #mkdir /home/namespace/box/data
 #mount --bind /tmp/ /home/namespace/box/data
+
+# Populate Proc Folder
+mkdir /home/namespace/box/proc
+mount -t proc proc /home/namespace/box/proc 
 
 # uncomment to protect PIDs
 #unshare --pid --fork --mount-proc=/home/namespace/box/proc \
