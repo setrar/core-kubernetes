6dab8e31f0c599b817eb0ccddc32bddfb3c8a587
setrar <setrar@valiha.com>
2022-06-26 10:49:05 -0400

    Update chroot0.sh

--- chapter-03/chroot0.sh
+++ chapter-03/chroot0.sh
@@ -17,4 +17,10 @@ mount -t proc proc /home/namespace/box/proc
 # mkdir /home/namespace/box/data
 # mount --bind /tmp/ /home/namespace/box/data
 
+# uncomment to protect PIDs
+#unshare --pid --fork --mount-proc=/home/namespace/box/proc \
 chroot /home/namespace/box /bin/bash
+
+# Uncomment to release
+#umount /home/namespace/box/proc
+#umount /home/namespace/box/data
